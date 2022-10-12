2abc8c4165598c2fd3477ca584ed49317971712b
Adian Fredifa <adian281r@gmail.com>
2022-10-12 04:23:49 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import DataGridEditable from "./components/table/DataGridEditable";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <DataGridEditable />
     </div>
   );
 }
